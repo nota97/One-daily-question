95c8a6e79fa8501fab37f5307b59132b701ff05a
gali <gali@galideMacBook-Pro.local>
2022-10-25 15:57:58 +0800

    update

--- oneDailyQuestion/leetcode/Solution0014/java/Solution.java
+++ oneDailyQuestion/leetcode/Solution0014/java/Solution.java
@@ -7,11 +7,24 @@ package oneDailyQuestion.Solution0014.java;
  */
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        return "";
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int len = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < len; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if ( i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
     }
 
 }
