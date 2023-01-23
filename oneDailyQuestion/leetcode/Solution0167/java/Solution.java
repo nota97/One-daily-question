fc89f444ba9d7626e75622c0f5f3678a85dc75c1
xbxu1 <xbxu1@dces.cn>
2023-01-23 16:21:23 +0800

    done

--- oneDailyQuestion/leetcode/Solution0167/java/Solution.java
+++ oneDailyQuestion/leetcode/Solution0167/java/Solution.java
@@ -1,2 +1,28 @@
-package oneDailyQuestion.Solution0167.java;public class Solution {
+package oneDailyQuestion.Solution0167.java;
+
+/**
+ * 167. 两数之和 II - 输入有序数组
+ * https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int s = numbers[left] + numbers[right];
+            if (s == target) {
+                return new int[]{left + 1, right + 1};
+            }
+            if (s < target) {
+                left++;
+            }
+            if (s > target) {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
